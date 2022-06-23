3113b79098614888c9c0f64e9d47c1add11d2506
PC Soft <senku@gmail.com>
2022-06-23 23:36:49 +0200

    Upload Our Project

--- resources/views/Doctor/home.blade.php
+++ resources/views/Doctor/home.blade.php
@@ -3,16 +3,7 @@
 <head>
 @include('Doctor.layout.bootstrap')
 @include('Doctor.layout.sidebar')
-<style>
-.middle {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
 
-</style>
 </head>
 <body>
  @include('Doctor.layout.sidebarMain')
